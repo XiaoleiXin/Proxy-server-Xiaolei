c9d86a550a8f4f1c98d2452fdbf87248c72467d5
XiaoleiXin <37600641+XiaoleiXin@users.noreply.github.com>
2018-06-17 17:56:58 -0700

    Put all the components using aws links.

--- server/index.js
+++ server/index.js
@@ -24,8 +24,13 @@ app.get('/:id/photosBundle.js', (req, res) => {
   fetchData(url, res);
 });
 
-app.get('/:id/reviewbundle.js', (req, res) => {
-  const url = 'http://localhost:3007/reviewbundle.js';
+app.get('/:id/reviewBundle.js', (req, res) => {
+  const url = 'http://gsgsignup-env.vynmi2jism.us-west-2.elasticbeanstalk.com/reviewBundle.js';
+  fetchData(url, res);
+});
+
+app.get('/:id/bundle.js', (req, res) => {
+  const url = `http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/${req.params.id}/bundle.js`;
   fetchData(url, res);
 });
 
@@ -46,22 +51,86 @@ app.get('/photos/:id', (req, res) => {
 });
 
 app.get('/house/:houseId/', (req, res) => {
-  const url = `http://localhost:3007/house/${req.params.houseId}`;
+  const url = `http://gsgsignup-env.vynmi2jism.us-west-2.elasticbeanstalk.com/house/${req.params.houseId}`;
   fetchData(url, res);
 });
 
 app.get('/house/:houseId/reviews', (req, res) => {
-  const url = `http://localhost:3007/house/${req.params.houseId}/reviews?page=${req.query.page}`;
+  const url = `http://gsgsignup-env.vynmi2jism.us-west-2.elasticbeanstalk.com/house/${req.params.houseId}/reviews?page=${req.query.page}`;
   fetchData(url, res);
 });
 
 app.get('/users', (req, res) => {
-  const url = 'http://localhost:3007/users';
+  const url = 'http://gsgsignup-env.vynmi2jism.us-west-2.elasticbeanstalk.com/users';
   fetchData(url, res);
 });
 app.get('/user/:userId', (req, res) => {
-  const url = `http://localhost:3007/user/${req.params.userId}`;
+  const url = `http://gsgsignup-env.vynmi2jism.us-west-2.elasticbeanstalk.com/user/${req.params.userId}`;
   fetchData(url, res);
+});
+
+app.get('/listing/:id', (req, res) => {
+  request('http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/listing/:id', function (error, response, body) { 
+    if (!error && response.statusCode === 200) {  
+      res.send(body); 
+    } 
+  });
+});
+
+app.get('/lists/:id', (req, res) => {
+  request('http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/lists/:id', function (error, response, body) { 
+    if (!error && response.statusCode === 200) {  
+      res.send(body); 
+    } 
+  });
+});
+
+app.get('/list/:id', (req, res) => {
+  request('http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/list/:id', function (error, response, body) { 
+    if (!error && response.statusCode === 200) {  
+      res.send(body); 
+    } 
+  });
+});
+
+app.get('/lists2listings/:id', (req, res) => {
+  request({url: 'http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/lists2listings/:id', qs: { listingIds: req.query.listingIds}}, function (error, response, body) { 
+    if (!error && response.statusCode === 200) {  
+      res.send(body); 
+    } 
+  });
+});
+
+app.get('/like/:id', (req, res) => {
+  request({url: 'http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/like/:id', qs: { data: req.query.data }}, function (error, response, body) { 
+    if (!error && response.statusCode === 200) {  
+      res.send(body); 
+    } 
+  });
+});
+
+app.post('/like', (req, res) => {
+  request.post('http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/like', {form:{data: req.body.data}}, (err, res) => { 
+    if (err) {
+      console.log(err);   
+    } else {
+      console.log('we did it') 
+    }
+  });
+});
+
+app.post('/lists', (req, res) => {
+  request.post('http://bnbclone2.mk2gvayuhh.us-east-1.elasticbeanstalk.com/lists', {form:{
+    listingId: req.body.listingId, 
+    listId: req.body.listId,
+    liked: req.body.liked}
+  }, (err, res) => { 
+    if (err) {
+      console.log(err);   
+    } else {
+      console.log('we did it lists') 
+    }
+  });
 });
 
 app.post('/dates/:id', (req, res) => {
